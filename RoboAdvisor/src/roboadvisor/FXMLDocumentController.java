5c6d318db14737dcf99ab6930b4b071a69be97b9
emes995 <edgar.carrera@outlook.com>
2017-02-04 01:02:47 -0500

    Added Dummy Optmizer Screen

--- RoboAdvisor/src/roboadvisor/FXMLDocumentController.java
+++ RoboAdvisor/src/roboadvisor/FXMLDocumentController.java
@@ -5,12 +5,19 @@
  */
 package roboadvisor;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
@@ -18,9 +25,28 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
      
+    @FXML Button optimizerBt;
+    
     @FXML
     private void onTreeNodeClick(ActionEvent event) {
         
+    }
+    
+    @FXML
+    private void onOptimizedBtClick(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("PortfolioOptimizer.fxml"));
+            Stage curStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Stage newStage = new Stage();
+            Scene curScene = new Scene(root);
+            newStage.setScene(curScene);
+            newStage.setTitle("Testing");
+            newStage.initModality(Modality.APPLICATION_MODAL);
+            newStage.showAndWait();
+            
+        } catch (IOException e) {
+            System.err.print(e);
+        }
     }
     
     @Override
